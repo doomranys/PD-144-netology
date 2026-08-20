9a1d409df5a9311cf64dbbead17baab767ba1a5c
Uskov.S.A <dantes994@mail.ru>
2026-08-20 15:39:09 +0300

    lesson 4: Iterators.Generators.Yield

---
+++ ADPY-148/dz_4/main.py
@@ -0,0 +1,77 @@
+import types
+
+
+# ============ Задание 1: Итератор ============
+class FlatIterator:
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+        self.outer_index = 0
+        self.inner_index = 0
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        while self.outer_index < len(self.list_of_list):
+            inner_list = self.list_of_list[self.outer_index]
+            
+            if self.inner_index < len(inner_list):
+                item = inner_list[self.inner_index]
+                self.inner_index += 1
+                return item
+            else:
+                self.outer_index += 1
+                self.inner_index = 0
+        
+        raise StopIteration
+
+
+# ============ Задание 2: Генератор ============
+def flat_generator(list_of_lists):
+    for outer_list in list_of_lists:
+        for item in outer_list:
+            yield item
+
+
+# ============ Тесты ============
+def test_1():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    
+    print("✅ Тест 1 (FlatIterator) пройден!")
+
+
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+    
+    print("✅ Тест 2 (flat_generator) пройден!")
+
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
+    print("\n🎉 Все тесты успешно пройдены!")
